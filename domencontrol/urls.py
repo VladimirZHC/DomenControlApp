61e3609565f435ae861ff276a148513f1c5576ea
Vladimir <Sluhaivova@gmail.com>
2022-04-18 13:59:33 +0300

    last update

--- domencontrol/urls.py
+++ domencontrol/urls.py
@@ -7,23 +7,12 @@ from . import settings
 from django.views.generic import TemplateView
 from django.conf.urls.static import static
 
-schema_view = get_schema_view(
-   openapi.Info(
-      title="GroupPolicyConstructor API",
-      default_version='v1',
-      contact=openapi.Contact(email="sluhaivova@gmail.com"),
-      license=openapi.License(name="BSD License"),
-   ),
-   public=True,
-   permission_classes=(permissions.AllowAny,),
-)
+
 
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
     path('', include('control.urls')),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('openapi/', TemplateView.as_view(template_name="index.html")),
 ] + static(settings.STATIC_URL)
